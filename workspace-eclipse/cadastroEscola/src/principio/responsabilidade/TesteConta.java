07195228f6be40d88ad242898162c3d7e037b6ec
João <Joo@DESKTOP-NHERT2Q>
2021-09-01 23:51:04 -0300

    principo da responsabilidade unica SOLID package .responsabilidade

---
+++ workspace-eclipse/cadastroEscola/src/principio/responsabilidade/TesteConta.java
@@ -0,0 +1,27 @@
+package principio.responsabilidade;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ContaBancaria bancaria = new ContaBancaria();
+		bancaria.setDescricao("Conta Bancaria do João");
+		
+		System.out.println(bancaria);
+		
+		bancaria.diminui100Reais();
+		System.out.println(bancaria);
+		
+		bancaria.sacarDinheiro(400);
+		System.out.println(bancaria);
+		
+		bancaria.depositoDinheiro(1000);
+		System.out.println(bancaria);
+		
+		bancaria.soma100Reais();
+		System.out.println(bancaria);
+		
+	}
+
+}
