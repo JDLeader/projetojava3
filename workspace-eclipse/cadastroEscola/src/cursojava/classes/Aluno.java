ea7ed69fc22803c798212b9eafbd34e2fec59798
João <Joo@DESKTOP-NHERT2Q>
2021-09-02 21:19:05 -0300

    Criando a super classe Pessoa

--- workspace-eclipse/cadastroEscola/src/cursojava/classes/Aluno.java
+++ workspace-eclipse/cadastroEscola/src/cursojava/classes/Aluno.java
@@ -6,23 +6,19 @@ import java.util.Objects;
 
 import cursojava.constantes.StatusAluno;
 
-public class Aluno {
+public class Aluno extends Pessoa {
 
-	private String nome;
-	private int idade;
-	private String dataNascimento;
-	private String registroGeral;
-	private String numeroCPF;
-	private String nomeMae;
-	private String nomePai;
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
-
+	
 	private List<Disciplina> disciplina = new ArrayList<Disciplina>();
 
 	public Aluno() {/* Cria dados na memoria - por padrão */
 
+	}
+	public Aluno (String nomePadrao) {
+		nome = nomePadrao;
 	}
 
 	public Aluno(String alunoVip, String alunoDataEspecial) {
@@ -38,62 +34,7 @@ public class Aluno {
 		this.disciplina = disciplina;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getRegistroGeral() {
-		return registroGeral;
-	}
-
-	public void setRegistroGeral(String registroGeral) {
-		this.registroGeral = registroGeral;
-	}
-
-	public String getNumeroCPF() {
-		return numeroCPF;
-	}
-
-	public void setNumeroCPF(String numeroCPF) {
-		this.numeroCPF = numeroCPF;
-	}
-
-	public String getNomeMae() {
-		return nomeMae;
-	}
-
-	public void setNomeMae(String nomeMae) {
-		this.nomeMae = nomeMae;
-	}
-
-	public String getNomePai() {
-		return nomePai;
-	}
-
-	public void setNomePai(String nomePai) {
-		this.nomePai = nomePai;
-	}
-
+	
 	public String getDataMatricula() {
 		return dataMatricula;
 	}
---
+++ workspace-eclipse/cadastroEscola/src/cursojava/classes/Diretor.java
@@ -0,0 +1,30 @@
+package cursojava.classes;
+
+public class Diretor extends Pessoa{
+	
+	private String registroEducacao;
+	private String tempoDirecacao;
+	private String titulacao;
+	
+	public String getRegistroEducacao() {
+		return registroEducacao;
+	}
+	public void setRegistroEducacao(String registroEducacao) {
+		this.registroEducacao = registroEducacao;
+	}
+	public String getTempoDirecacao() {
+		return tempoDirecacao;
+	}
+	public void setTempoDirecacao(String tempoDirecacao) {
+		this.tempoDirecacao = tempoDirecacao;
+	}
+	public String getTitulacao() {
+		return titulacao;
+	}
+	public void setTitulacao(String titulacao) {
+		this.titulacao = titulacao;
+	}
+	
+	
+
+}
