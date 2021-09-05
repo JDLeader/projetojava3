ccaa45c435992560e803c36b4a666c602e3c1b88
João <Joo@DESKTOP-NHERT2Q>
2021-09-04 23:50:28 -0300

    Simulando uma Thread

--- workspace-eclipse/cadastroEscola/src/crso/java/thread/AulaThread.java
+++ workspace-eclipse/cadastroEscola/src/crso/java/thread/AulaThread.java
@@ -7,15 +7,29 @@ public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		/*processamento paralelo*/
-		for(int pos = 0; pos < 10; pos++) {
-						
-			/* Executar envio com tempo de parada*/
-			System.out.println("Executando rotina");
-			
-			Thread.sleep(1000);/*Um segundo para cada linha*/
-		}
+		/*thread processando em paralelo*/
+		new Thread() {
+			public void run() {
+
+				/*processamento paralelo*/
+				for(int pos = 0; pos < 10; pos++) {
+								
+					/* Executar envio com tempo de parada*/
+					System.out.println("Executando rotina");
+					
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}/*Um segundo para cada linha*/
+				}
+
+				
+			}
+		}.start();/*Ligando a Thread paralelamente*/
 		
+			
 		
 		System.out.println("Fim do teste de Thread");
 		/*Exemplo de fluxo do sistema*/
